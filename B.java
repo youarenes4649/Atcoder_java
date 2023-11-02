dd7a5d4e9ece924a4b76b54c27c3b8c1f2320643
立見有 <tatsumiyuu@tatsumiyuunoMacBook-Air.local>
2023-11-02 11:53:02 +0900

    Atcoder ABC326 B

---
+++ B.java
@@ -0,0 +1,24 @@
+package Atcoder_java;
+
+import java.util.*;
+
+public class B{
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = N; i < 1000; i++){
+            int a = i / 100;
+            int b = (i / 10) % 10;
+            int c = i % 10;
+
+            if (a * b == c){
+                System.out.println((i));
+                break;
+            }
+        }
+
+
+    }
+}
